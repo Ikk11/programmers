36ead3f0b20b99559c9d5a7c5c59822e301c0642
Ikk11 <142803910+Ikk11@users.noreply.github.com>
2023-09-10 15:03:16 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,48 +1,6 @@
 class Solution {
-    public long power2(int x) {
-        long ans = 1;
-        for (int i = 0; i < x; i++) {
-            ans *= 2;
-        }
-        return ans;
-    }
-    
-    public long power10(int x) {
-        long ans = 1;
-        for (int i = 0; i < x; i++) {
-            ans *= 10;
-        }
-        return ans;
-    }
-    
-    
     public String solution(String bin1, String bin2) {
-        long bin01 = Long.parseLong(bin1);
-        long bin02 = Long.parseLong(bin2);
-        
-        int tmp1 = 0;
-        int bin11 = 0;
-        while (bin01 > 0) {
-            bin11 += bin01%10*power2(tmp1++);
-            bin01 /= 10;
-        }
-        int tmp2 = 0;
-        int bin22 = 0;
-        while (bin02 > 0) {
-            bin22 += bin02%10*power2(tmp2++);
-            bin02 /= 10;
-        }
-        
-        int bin = bin11 + bin22;
-        
-        long answer = 0;
-        for (int i = 12; i > 0; i--) {
-            if (power2(i-1) <= bin && bin < power2(i)) {
-                answer += power10(i-1);
-                bin -= power2(i-1);
-            }
-        }
-        String ans = String.valueOf(answer);
-        return ans;
+        String answer = Integer.toString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2) ,2);
+        return answer;
     }
 }
