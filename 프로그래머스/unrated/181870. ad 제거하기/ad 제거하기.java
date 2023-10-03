bae19f63afb34b9dd6d7c558c2bba79bb3531745
Ikk11 <142803910+Ikk11@users.noreply.github.com>
2023-10-03 18:09:31 +0900

    [unrated] Title: ad 제거하기, Time: 0.51 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < strArr.length; i++) {
+            if (strArr[i].indexOf("ad") == -1) {
+                arr.add(strArr[i]);
+            }
+        }
+        String[] answer = arr.toArray(new String[arr.size()]);
+        return answer;
+    }
+}
