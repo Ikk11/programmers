015bf168956a8c6b1e7b0a73aa1e7317f0708bff
Ikk11 <142803910+Ikk11@users.noreply.github.com>
2023-10-22 20:57:12 +0900

    [level unrated] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+class Solution {
+    int gsd(int a, int b) {
+        if (a%b == 0) {
+            return b;
+        }
+        return (gsd(b, a%b));
+    }
+    int solution(int a, int b) {
+        int gsd_b = b/gsd(a, b);
+        while (gsd_b%2 == 0) {
+            gsd_b /= 2;
+        }
+        while (gsd_b%5 == 0) {
+            gsd_b /= 5;
+        }
+        if (gsd_b == 1) {
+            return 1;
+        } else {
+            return 2;            
+        }
+    }
+}
